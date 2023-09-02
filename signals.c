83491ddd658fd083dfb7d96016a2668310a3c83c
Salvador Azcon Garcia <saazcon-@c1r3s5.42madrid.com>
2023-09-02 08:37:21 +0200

    prueba

--- signals.c
+++ signals.c
@@ -6,7 +6,7 @@
 /*   By: saazcon- <saazcon-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 14:34:30 by cyacoub-          #+#    #+#             */
-/*   Updated: 2023/09/02 08:29:40 by saazcon-         ###   ########.fr       */
+/*   Updated: 2023/09/02 08:37:05 by saazcon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,25 +51,25 @@ void	sig_ignore(void)
 	signal(SIGQUIT, SIG_IGN);
 }
 
-/* void	heredoc_signal(int signal)
-{
-	g_minishell.signal = signal;
-	g_minishell.heredoc = true;
-	set_env(&g_minishell.envs, "?",
-		ft_itoa(128 + g_minishell.signal));
-}
+// void	heredoc_signal(int signal)
+// {
+// 	g_minishell.signal = signal;
+// 	g_minishell.heredoc = true;
+// 	set_env(&g_minishell.envs, "?",
+// 		ft_itoa(128 + g_minishell.signal));
+// }
 
-void	cmd_signal(int signal)
-{
-	g_minishell.signal = signal;
-	if (signal == SIGINT)
-	{
-		write(1, "\n", 1);
-		rl_on_new_line();
-		rl_replace_line("", 0);
-	}
-	if (signal == SIGQUIT)
-	{
-		ft_putendl_fd("Quit", STDERR_FILENO);
-	}
-} */
+// void	cmd_signal(int signal)
+// {
+// 	g_minishell.signal = signal;
+// 	if (signal == SIGINT)
+// 	{
+// 		write(1, "\n", 1);
+// 		rl_on_new_line();
+// 		rl_replace_line("", 0);
+// 	}
+// 	if (signal == SIGQUIT)
+// 	{
+// 		ft_putendl_fd("Quit", STDERR_FILENO);
+// 	}
+// } 
